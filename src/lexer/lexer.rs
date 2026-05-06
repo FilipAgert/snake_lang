7d842104155da85deee886d4117212eccb5b84c1
Filip Agert <filip.agert@gmail.com>
2026-05-06 22:58:56 +0200

    might have wrote the lexer

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,47 +1,113 @@
-use crate::lexer::token::{Keyword, Operator, Symbol, Token, TokenType};
+use crate::lexer::token::{Token, TokenType};
 use std::iter::Peekable;
+use std::mem;
 use std::str::Chars;
+use std::string::String;
 
-pub struct Lexer<'a> {
-    cursor: Peekable<Chars<'a>>,
+#[derive(Debug)]
+struct TokenStr {
+    string: String,
+    line: u32,
+    col: u32,
+}
+impl TokenStr {
+    fn new(string: String, line: u32, col: u32) -> Self {
+        TokenStr { string, line, col }
+    }
 }
 
-impl<'a> Lexer<'a> {
-    pub fn new(s: &'a str) -> Lexer {
-        Lexer {
-            cursor: s.chars().peekable(),
+const SPECIAL_SYMBOLS: &'static str = "()[]{};=+-/*";
+const IGNORE_CHARACTERS: &'static str = "\n\r";
+
+pub fn scan(str: &str) -> Vec<Token> {
+    let token_str = seperate_string(str);
+    let mut tokens = Vec::new();
+    for t_str in token_str {
+        let t_type = TokenType::from_str(&t_str.string);
+        let token = Token {
+            token_type: t_type,
+            line: t_str.line,
+            col: t_str.col,
+            len: t_str
+                .string
+                .len()
+                .try_into()
+                .expect("Should work to convert usize into u32"),
+        };
+        tokens.push(token);
+    }
+    tokens
+}
+
+fn seperate_string(str: &str) -> Vec<TokenStr> {
+    let mut cursor = str.chars().peekable();
+    let mut strings: Vec<TokenStr> = Vec::new();
+    let mut curr_str: String = String::new();
+    let mut line_ctr = 0;
+    let mut col_ctr = 0;
+    let mut start_col = 0;
+
+    while let Some(c) = cursor.next() {
+        col_ctr += 1;
+        if c == '\n' {
+            line_ctr += 1;
+            col_ctr = 0;
+            continue;
+        }
+
+        if (c == ' ' || c == '\n') && !curr_str.is_empty() {
+            let str_token = TokenStr::new(mem::take(&mut curr_str), line_ctr, start_col);
+            strings.push(str_token);
+            start_col = col_ctr;
+        } else if SPECIAL_SYMBOLS.contains(c) {
+            if !curr_str.is_empty() {
+                let str_token = TokenStr::new(mem::take(&mut curr_str), line_ctr, start_col);
+                strings.push(str_token);
+            }
+            strings.push(TokenStr::new(c.to_string(), line_ctr, col_ctr));
+            start_col = col_ctr;
+        } else if c != ' ' {
+            curr_str.push(c);
         }
     }
-
-    pub fn scan(self: Lexer<'a>) -> Vec<Token> {
-        let tokens = Vec::new();
-
-        tokens
+    if !curr_str.is_empty() {
+        strings.push(TokenStr::new(curr_str, line_ctr, start_col));
     }
 
-    fn advance_token(mut self: Lexer<'a>) -> Token {
-        while let Some(c) = self.advance_char() {
-            match c {}
-        }
-
-        Token::new(TokenType::EOF)
-    }
-
-    fn advance_char(mut self: Lexer<'a>) -> Option<char> {
-        self.cursor.next()
-    }
+    strings
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::lexer::token::{Operator, Symbol, Token};
+    use crate::lexer::token::{Operator, Token};
 
     #[test]
-    fn test_scan_operators() {
+    fn test_sep_strings() {
+        let input = "int val = 4;\nint x = f(4);";
+        let result = seperate_string(input);
+        assert_eq!(result[0].string, "int");
+        assert_eq!(result[1].string, "val");
+        assert_eq!(result[2].string, "=");
+        assert_eq!(result[3].string, "4");
+        assert_eq!(result[4].string, ";");
+        assert_eq!(result[5].string, "int");
+        assert_eq!(result[6].string, "x");
+        assert_eq!(result[7].string, "=");
+        assert_eq!(result[8].string, "f");
+        assert_eq!(result[9].string, "(");
+        assert_eq!(result[10].string, "4");
+        assert_eq!(result[11].string, ")");
+        assert_eq!(result[12].string, ";");
+        for res in result {
+            println!("{:?}", res)
+        }
+    }
+
+    #[test]
+    fn test_scan() {
         let input = "+ =";
-        let lexer = Lexer::new(input);
-        let result = lexer.scan();
+        let result = scan(input);
         assert_eq!(result[0].token_type, TokenType::Op(Operator::Plus));
         assert_eq!(result[1].token_type, TokenType::Op(Operator::Equal));
     }
--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -7,8 +7,8 @@ pub struct Token {
 }
 
 impl Token {
-    pub fn new(type_t: TokenType) -> Token {
-        Token {
+    pub fn new(type_t: TokenType) -> Self {
+        Self {
             token_type: type_t,
             col: 0,
             line: 0,
@@ -22,8 +22,29 @@ pub enum TokenType {
     Identifier(String),
     Op(Operator),
     Integer(i64),
-    Symbol(Symbol),
+    Bracket(Bracket),
     EOF,
+}
+
+impl TokenType {
+    pub fn from_str(str: &str) -> Self {
+        if str.len() == 1 {
+            let ch = str.chars().next().expect("Already checked length");
+            if let Some(op) = Operator::from_ch(ch) {
+                return TokenType::Op(op);
+            }
+            if let Some(bracket) = Bracket::from_ch(ch) {
+                return TokenType::Bracket(bracket);
+            }
+        }
+        if let Some(keyword) = Keyword::from_str(str) {
+            return TokenType::Keyword(keyword);
+        }
+        if let Ok(int) = str.parse::<i64>() {
+            return TokenType::Integer(int);
+        }
+        TokenType::Identifier(str.to_string())
+    }
 }
 #[derive(Debug, PartialEq)]
 pub enum Keyword {
@@ -36,8 +57,40 @@ pub enum Operator {
     Equal, // =
 }
 
+impl Operator {
+    pub fn from_ch(c: char) -> Option<Operator> {
+        match c {
+            '=' => Some(Operator::Equal),
+            '+' => Some(Operator::Plus),
+            _ => None,
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
-pub enum Symbol {}
+pub enum Bracket {
+    Parenthesis(Side),
+    Square(Side),
+    CurlyBrace(Side),
+}
+impl Bracket {
+    pub fn from_ch(c: char) -> Option<Bracket> {
+        match c {
+            '(' => Some(Bracket::Parenthesis(Side::Left)),
+            ')' => Some(Bracket::Parenthesis(Side::Right)),
+            '[' => Some(Bracket::Square(Side::Left)),
+            ']' => Some(Bracket::Square(Side::Right)),
+            '{' => Some(Bracket::CurlyBrace(Side::Left)),
+            '}' => Some(Bracket::CurlyBrace(Side::Right)),
+            _ => None,
+        }
+    }
+}
+#[derive(Debug, PartialEq)]
+pub enum Side {
+    Left,
+    Right,
+}
 
 impl Keyword {
     pub fn from_str(s: &str) -> Option<Keyword> {
