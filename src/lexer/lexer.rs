f7f44a2c68faa9206861d39569e3afe77109cf9e
Filip Agert <filip.agert@gmail.com>
2026-05-06 21:12:16 +0200

    mid level commit

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,9 +1,35 @@
 use crate::lexer::token::{Keyword, Operator, Symbol, Token, TokenType};
+use std::iter::Peekable;
+use std::str::Chars;
 
-pub fn scan(s: &str) -> Vec<Token> {
-    let tokens = Vec::new();
+pub struct Lexer<'a> {
+    cursor: Peekable<Chars<'a>>,
+}
 
-    tokens
+impl<'a> Lexer<'a> {
+    pub fn new(s: &'a str) -> Lexer {
+        Lexer {
+            cursor: s.chars().peekable(),
+        }
+    }
+
+    pub fn scan(self: Lexer<'a>) -> Vec<Token> {
+        let tokens = Vec::new();
+
+        tokens
+    }
+
+    fn advance_token(mut self: Lexer<'a>) -> Token {
+        while let Some(c) = self.advance_char() {
+            match c {}
+        }
+
+        Token::new(TokenType::EOF)
+    }
+
+    fn advance_char(mut self: Lexer<'a>) -> Option<char> {
+        self.cursor.next()
+    }
 }
 
 #[cfg(test)]
@@ -14,7 +40,8 @@ mod tests {
     #[test]
     fn test_scan_operators() {
         let input = "+ =";
-        let result = scan(input);
+        let lexer = Lexer::new(input);
+        let result = lexer.scan();
         assert_eq!(result[0].token_type, TokenType::Op(Operator::Plus));
         assert_eq!(result[1].token_type, TokenType::Op(Operator::Equal));
     }
--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -2,8 +2,19 @@
 pub struct Token {
     pub token_type: TokenType,
     pub line: u32,
-    pub col: u8,
-    pub len: u8,
+    pub col: u32,
+    pub len: u32,
+}
+
+impl Token {
+    pub fn new(type_t: TokenType) -> Token {
+        Token {
+            token_type: type_t,
+            col: 0,
+            line: 0,
+            len: 0,
+        }
+    }
 }
 #[derive(Debug, PartialEq)]
 pub enum TokenType {
